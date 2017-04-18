5e27a6a3af4ffca8435719d82e1041d099a2a231
wildskyf <poppin.wildsky+gh@gmail.com>
2017-04-18 23:50:19 +0800

    migrate to webExtension

--- index.js
+++ index.js
@@ -1,58 +1,17 @@
-const {Cc, Ci, Cm, Cr, Cu, CC, components} = require("chrome");
-const {NewTabURL} = require('resource:///modules/NewTabURL.jsm');
-const ogd4tab = NewTabURL.get();
-
-Cm.QueryInterface(Ci.nsIComponentRegistrar);
-Cu.import("resource://gre/modules/XPCOMUtils.jsm");
-Cu.import("resource://gre/modules/Services.jsm");
-
-// globals
-var factory = undefined;
-const _name = 'rancolor';
-const _description = 'This page provides some random colors';
-const _id = 'aa132730-2278-11e5-867f-0800200c9a66'; // https://www.famkruithof.net/uuid/uuidgen
-const _url = `about:${_name}`;
-const _src = "resource://colortab/data/index.html";
-
-function AboutCustom() {};
-AboutCustom.prototype = Object.freeze({
-	classDescription: _description,
-	contractID: '@mozilla.org/network/protocol/about;1?what=' + _name,
-	classID: components.ID(`{${_id}}`),
-	QueryInterface: XPCOMUtils.generateQI([Ci.nsIAboutModule]),
-
-	getURIFlags: aURI => Ci.nsIAboutModule.ALLOW_SCRIPT,
-
-	newChannel: (aURI, aSecurity_or_aLoadInfo) => {
-		let uri = Services.io.newURI(_src, null, null);
-		var channel = Services.io.newChannelFromURIWithLoadInfo(uri, aSecurity_or_aLoadInfo);
-		channel.originalURI = aURI;
-		return channel;
+var replaceNewTabPage = tab => {
+	if (tab.url == "about:newtab") {
+		browser.tabs.update(
+			tab.id,
+			{ url:"data/index.html" }
+		)
+		/*
+		browser.tabs.remove(tab.id);
+		browser.tabs.create({
+			url:"data/index.html"
+		});
+		*/
 	}
-});
+};
 
-function Factory(component) {
-	this.createInstance = (outer, iid) => {
-		if (outer)
-			throw Cr.NS_ERROR_NO_AGGREGATION;
-		return new component();
-	};
-	this.register = () =>
-		Cm.registerFactory(component.prototype.classID, component.prototype.classDescription, component.prototype.contractID, this);
-	this.unregister = () =>
-		Cm.unregisterFactory(component.prototype.classID, this);
-
-	Object.freeze(this);
-	this.register();
-}
-
-exports.main = () => {
-	factory = new Factory(AboutCustom);
-	NewTabURL.override(_url);
-}
-
-exports.onUnload = () => {
-	factory.unregister();
-	NewTabURL.override(ogd4tab);
-}
+browser.tabs.onCreated.addListener(replaceNewTabPage);
 
