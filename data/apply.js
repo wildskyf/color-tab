f99b86b3aba0a51025cc17c90faa604b115f34fa
wildskyf <poppin.wildsky+gh@gmail.com>
2017-06-25 01:18:17 +0800

    replace colors.js with colors.json

--- data/apply.js
+++ data/apply.js
@@ -1,25 +1,29 @@
 window.onload = () => {
-	var color = colors[Math.floor(Math.random() * colors.length)];
+	const colors_url = chrome.extension.getURL('data/colors.json');
+	const toJSON = res => res.json();
 
-	var header = document.querySelector("header");
-	var h1 = document.createElement("h1");
-	var a = document.createElement("a");
+	fetch(colors_url).then(toJSON).then(colors => {
+		var color = colors[Math.floor(Math.random() * colors.length)];
+
+		var header = document.querySelector("header");
+		var h1 = document.createElement("h1");
+		var a = document.createElement("a");
 		a.href = color.url;
-		a.appendChild(document.createTextNode(`${color.userName} - ${color.title}`));
+		a.textContent = `${color.userName} - ${color.title}`;
 
-	var max_length = 40;
-	var p = document.createElement("p");
-	p.appendChild(document.createTextNode(
-		color.description.slice(0,max_length) + (color.description.length > max_length? "…":"")));
+		var max_length = 100;
+		var p = document.createElement("p");
+		p.textContent = color.description.slice(0,max_length) + (color.description.length > max_length? "…":"")
 
-	header.appendChild(h1).appendChild(a);
-	header.appendChild(p);
+		header.appendChild(h1).appendChild(a);
+		header.appendChild(p);
 
-	Array.from(document.querySelector("#palette ul").children).forEach((load, i) => {
-		var div = document.createElement("div");
+		Array.from(document.querySelector("#palette ul").children).forEach((load, i) => {
+			var div = document.createElement("div");
 			div.className = "color-hex";
-			div.appendChild(document.createTextNode(`#${color.colors[i]}`));
-		load.appendChild(div);
-		load.style.backgroundColor = "#" + color.colors[i];
+			div.textContent = `#${color.colors[i]}`;
+			load.appendChild(div);
+			load.style.backgroundColor = "#" + color.colors[i];
+		});
 	});
 }
